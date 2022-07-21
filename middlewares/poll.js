3bc9950835273ef3b9c98d1464ee854caacd9239
srkds <im.nirav86@gmail.com>
2022-07-21 22:02:20 +0530

    feat: get poll by id middleware added

--- middlewares/poll.js
+++ middlewares/poll.js
@@ -1,4 +1,16 @@
+const poll = require("../models/poll");
+
 exports.setOptionId = (req, res, next, id) => {
   req.optionId = id;
   next();
+};
+
+exports.getPollById = (req, res, next, id) => {
+  poll.findById(id).exec((err, poll) => {
+    if (err || !poll) {
+      return res.status(400).json({ error: "Poll Not Found" });
+    }
+    req.poll = poll;
+    next();
+  });
 };
